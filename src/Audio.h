5e5bb9e73328cbcd1b7e0aa1d3225d11d952ea22
nathanbc32 <2030699@apps.nsd.org>
2026-05-23 19:28:10 -0700

    Create Audio class

---
+++ src/Audio.h
@@ -0,0 +1,21 @@
+#ifndef AUDIO_H
+#define AUDIO_H
+
+#include <Arduino.h>
+#include <AudioTools.h>
+
+class Audio
+{
+private:
+    /* data */
+public:
+    Audio(/* args */);
+    ~Audio();
+};
+
+Audio::Audio(/* args */)
+{
+}
+
+
+#endif //AUDIO_H
